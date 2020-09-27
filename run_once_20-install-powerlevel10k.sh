81ff78ddf6b00897cdabfecc3c2a3f8c0e8be9e3
Stuart Campbell <stuart@stuartcampbell.me>
2020-09-26 22:34:31 -0400

    Added powerlevel10k

---
+++ run_once_20-install-powerlevel10k.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+version="801bfbb2946b10527e75e77838a3657d78cb92e5"
+destdir="${HOME}/.oh-my-zsh/custom/themes/powerlevel10k"
+
+rm -rf ${destdir}
+mkdir -p ${destdir}
+curl -s -L https://github.com/romkatv/powerlevel10k/archive/${version}.tar.gz | tar -C ${destdir} --strip-components=1 -xzf -
