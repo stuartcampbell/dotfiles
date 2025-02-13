65de274f602567562ea313e0d2a9b7e8591cc617
Stuart Campbell <scampbell@bnl.gov>
2025-02-13 16:48:24 -0500

    Add uv/uvx shell completions for fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
 
+    # UV/UVX
+    uv generate-shell-completion fish | source
+    uvx generate-shell-completion fish | source
+
     # Automatically "Warpify" subshells
     printf '\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "fish"}}\x9c'
 
 end
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/scampbell/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
